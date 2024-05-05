d665ee6e792b78aa5b29b1742ccec4048b83e991
奈良本悠二 <naramotoyuuji@OMBP-M2P.local>
2024-05-06 07:42:18 +0900

    feat: 🎸 null-ls.lua
    change local formatter path of rustformat

--- .config/nvim/lua/configs/null-ls.lua
+++ .config/nvim/lua/configs/null-ls.lua
@@ -1,40 +1,40 @@
 local M = {}
 
 function M.setup()
-  local null_ls = require("null-ls")
-  local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-  null_ls.setup({
-    sources = {
-      null_ls.builtins.formatting.fish_indent,
-      null_ls.builtins.formatting.stylua,
-      null_ls.builtins.formatting.prettierd,
-      null_ls.builtins.formatting.dart_format,
-      null_ls.builtins.formatting.rustfmt.with({
-        prefer_local = "~/.cargo/bin/rustfmt",
-      }),
-      null_ls.builtins.diagnostics.eslint.with({
-        prefer_local = "node_modules/.bin", --プロジェクトローカルがある場合はそれを利用
-      }),
-      null_ls.builtins.diagnostics.hadolint,
-      null_ls.builtins.completion.spell,
-      null_ls.builtins.completion.luasnip,
-    },
-    on_attach = function(client, bufnr)
-      if client.supports_method("textDocument/formatting") then
-        vim.api.nvim_clear_autocmds({ group = augroup })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-          group = augroup,
-          buffer = bufnr,
-          callback = function()
-            -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-            vim.lsp.buf.format({ async = false })
-            -- vim.lsp.buf.formatting_sync()
-          end,
-        })
-      end
-    end,
-    debug = false,
-  })
+	local null_ls = require("null-ls")
+	local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+	null_ls.setup({
+		sources = {
+			null_ls.builtins.formatting.fish_indent,
+			null_ls.builtins.formatting.stylua,
+			null_ls.builtins.formatting.prettierd,
+			null_ls.builtins.formatting.rustfmt.with({
+				prefer_local = "~/.local/share/mise/installs/rust/latest/bin/rustfmt",
+			}),
+			null_ls.builtins.diagnostics.eslint.with({
+				prefer_local = "node_modules/.bin", --プロジェクトローカルがある場合はそれを利用
+			}),
+			null_ls.builtins.diagnostics.hadolint,
+			null_ls.builtins.completion.spell,
+			null_ls.builtins.completion.luasnip,
+			null_ls.builtins.formatting.ast_grep,
+		},
+		on_attach = function(client, bufnr)
+			if client.supports_method("textDocument/formatting") then
+				vim.api.nvim_clear_autocmds({ group = augroup })
+				vim.api.nvim_create_autocmd("BufWritePre", {
+					group = augroup,
+					buffer = bufnr,
+					callback = function()
+						-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+						vim.lsp.buf.format({ async = false })
+						-- vim.lsp.buf.formatting_sync()
+					end,
+				})
+			end
+		end,
+		debug = false,
+	})
 end
 
 return M
