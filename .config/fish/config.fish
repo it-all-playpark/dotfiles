7800e36b4d5c45233b031918f3749dedbb2dc905
奈良本悠二 <naramotoyuuji@OMBP-M2P.local>
2024-05-07 07:10:31 +0900

    feat: 🎸 add mise path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -65,7 +65,7 @@ function yy
 end
 
 # PATH設定
-set -gx fish_user_paths /opt/homebrew/bin /opt/homebrew/sbin /usr/bin/php ~/ghq/github.com/astj/ghq-migrator ~/google-cloud-sdk/bin ~/flutter/bin ~/Library/Android/sdk ~/.cargo/bin $fish_user_paths
+set -gx fish_user_paths /opt/homebrew/bin /opt/homebrew/sbin /usr/bin/php ~/ghq/github.com/astj/ghq-migrator ~/google-cloud-sdk/bin ~/Library/Android/sdk ~/.local/share/mise/shims $fish_user_paths
 # 重複を削除
 set -U fish_user_paths (echo $fish_user_paths | tr ' ' '\n' | sort -u)
 
