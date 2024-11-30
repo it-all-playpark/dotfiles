2cc0f7eba3dd769385b5cc979319c615e28652f2
yuji naramoto <yuji.naramoto@playpark.work>
2024-11-30 11:23:09 +0900

    feat: 🎸 changu keymap to 024

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,6 +1,24 @@
 local map = vim.api.nvim_set_keymap
 -- global
-map("n", "<leader>l", "<cmd>bn<CR>", { noremap = true, silent = true })
-map("n", "<leader>h", "<cmd>bp<CR>", { noremap = true, silent = true })
-map("n", "<leader>k", "<cmd>b#<CR>", { noremap = true, silent = true })
-map("n", "<leader>j", "<cmd>bd<CR>", { noremap = true, silent = true })
+-- ノーマルモードのキーマッピング
+map("n", "h", "n", { noremap = true, silent = true })           -- next
+map("n", "H", "N", { noremap = true, silent = true })           -- previous
+map("n", "k", "h", { noremap = true, silent = true })           -- left
+map("n", "t", "j", { noremap = true, silent = true })           -- down
+map("n", "n", "k", { noremap = true, silent = true })           -- up
+map("n", "s", "l", { noremap = true, silent = true })           -- right
+-- ビジュアルモードのキーマッピング
+map("v", "k", "h", { noremap = true, silent = true })           -- left
+map("v", "t", "j", { noremap = true, silent = true })           -- down
+map("v", "n", "k", { noremap = true, silent = true })           -- up
+map("v", "s", "l", { noremap = true, silent = true })           -- right
+-- インサートモードのカーソル移動
+map("i", "<C-k>", "<Left>", { noremap = true, silent = true })  -- left
+map("i", "<C-t>", "<Down>", { noremap = true, silent = true })  -- down
+map("i", "<C-n>", "<Up>", { noremap = true, silent = true })    -- up
+map("i", "<C-s>", "<Right>", { noremap = true, silent = true }) -- right
+
+map("n", "<leader>k", "<cmd>bp<CR>", { noremap = true, silent = true })
+map("n", "<leader>t", "<cmd>bd<CR>", { noremap = true, silent = true })
+map("n", "<leader>s", "<cmd>bn<CR>", { noremap = true, silent = true })
+map("n", "<leader>n", "<cmd>b#<CR>", { noremap = true, silent = true })
