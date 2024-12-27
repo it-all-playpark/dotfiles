ba49f9fd5886c9ed59418646a37ab6f033aa511b
yuji naramoto <yuji.naramoto@playpark.work>
2024-12-28 08:48:41 +0900

    feat: 🎸 change dl command to realtime

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -48,7 +48,7 @@ abbr lp lpass show \$\(lpass ls -l \| fzf \| awk \'{print \$5}\' \| sd \']\$\' \
 # 選択した起動中コンテナに入る
 abbr d docker exec -it \$\(docker ps \| fzf --layout=reverse --prompt \"Docker Container\>\" --preview-window \'bottom:70%\' --preview \'docker logs \{1\}\'\| awk \'\{print \$1\}\'\) sh
 # 選択したlogを表示する
-abbr dl docker logs --details \$\(docker ps -a \| fzf --layout=reverse --prompt \"Docker Container\>\" --preview-window \'bottom:70%\' --preview \'docker logs --details \{1\}\' \| awk \'\{print \$1\}\'\)
+abbr dl docker logs --follow --tail\=100 \$\(docker ps -a \| fzf --layout=reverse --prompt \"Docker Container\>\" --preview-window \'bottom:70%\' --preview \'docker logs --details \{1\}\' \| awk \'\{print \$1\}\'\)
 # 選択したコンテナを削除する
 abbr dr docker rm \$\(docker ps -a \| fzf --layout=reverse --prompt \"Docker Container\>\" --preview-window \'bottom:70%\' --preview \'docker logs \{1\}\'\| awk \'\{print \$1\}\'\)
 # 選択したコンテナイメージを削除する
