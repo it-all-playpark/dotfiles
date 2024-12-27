3a2d7930242a71d4df25646ca1a7fa7b60fafb95
yuji naramoto <yuji.naramoto@playpark.work>
2024-12-27 17:17:56 +0900

    feat: 🎸 direction keys: ktns -> tnrs

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -3,26 +3,26 @@ local map = vim.api.nvim_set_keymap
 -- ノーマルモードのキーマッピング
 map("n", "h", "n", { noremap = true, silent = true })           -- next
 map("n", "H", "N", { noremap = true, silent = true })           -- previous
-map("n", "k", "h", { noremap = true, silent = true })           -- left
-map("n", "t", "j", { noremap = true, silent = true })           -- down
-map("n", "n", "k", { noremap = true, silent = true })           -- up
+map("n", "t", "h", { noremap = true, silent = true })           -- left
+map("n", "n", "j", { noremap = true, silent = true })           -- down
+map("n", "r", "k", { noremap = true, silent = true })           -- up
 map("n", "s", "l", { noremap = true, silent = true })           -- right
 map("n", "U", "<C-r>", { noremap = true, silent = true })       -- redo
 -- ビジュアルモードのキーマッピング
-map("v", "k", "h", { noremap = true, silent = true })           -- left
-map("v", "t", "j", { noremap = true, silent = true })           -- down
-map("v", "n", "k", { noremap = true, silent = true })           -- up
+map("v", "t", "h", { noremap = true, silent = true })           -- left
+map("v", "n", "j", { noremap = true, silent = true })           -- down
+map("v", "r", "k", { noremap = true, silent = true })           -- up
 map("v", "s", "l", { noremap = true, silent = true })           -- right
 -- インサートモードのカーソル移動
-map("i", "<C-k>", "<Left>", { noremap = true, silent = true })  -- left
-map("i", "<C-t>", "<Down>", { noremap = true, silent = true })  -- down
-map("i", "<C-n>", "<Up>", { noremap = true, silent = true })    -- up
+map("i", "<C-t>", "<Left>", { noremap = true, silent = true })  -- left
+map("i", "<C-n>", "<Down>", { noremap = true, silent = true })  -- down
+map("i", "<C-r>", "<Up>", { noremap = true, silent = true })    -- up
 map("i", "<C-s>", "<Right>", { noremap = true, silent = true }) -- right
 
-map("n", "<leader>k", "<cmd>bp<CR>", { noremap = true, silent = true })
-map("n", "<leader>t", "<cmd>bd<CR>", { noremap = true, silent = true })
+map("n", "<leader>t", "<cmd>bp<CR>", { noremap = true, silent = true })
+map("n", "<leader>n", "<cmd>bd<CR>", { noremap = true, silent = true })
 map("n", "<leader>s", "<cmd>bn<CR>", { noremap = true, silent = true })
-map("n", "<leader>n", "<cmd>b#<CR>", { noremap = true, silent = true })
+map("n", "<leader>r", "<cmd>b#<CR>", { noremap = true, silent = true })
 
 -------------
 -- plugins --
