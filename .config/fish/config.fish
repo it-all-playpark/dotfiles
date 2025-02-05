689053fe64c64e7ee5ba35b47a25feaf8940e7e9
yuji naramoto <yuji.naramoto@playpark.work>
2025-02-05 14:29:53 +0900

    feat: 🎸 add fl and flc command

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -36,6 +36,10 @@ abbr ld lazydocker
 # カレントディレクトリのパスをクリップボードにコピー 
 abbr pwdc echo -n \"\$\(pwd\)\" \| pbcopy
 
+# カレントディレクトリ配下の指定したディレクトリ配下のファイルパスと中身を一括取得
+abbr fl find \$\(eza -DR \| sed \'s\/\:\$\/\/\' \| grep \'\^\.\/\' \| fzf\) -type f -exec echo \"==== {} ====\" \\\; -exec cat {} \\\;
+abbr flc find \$\(eza -DR \| sed \'s\/\:\$\/\/\' \| grep \'\^\.\/\' \| fzf\) -type f -exec echo \"==== {} ====\" \\\; -exec cat {} \\\; \| pbcopy
+
 # MySqlのDBを選択して接続
 abbr mdb cat ~/.myclirc ~/.myclirc.local \> ~/.myclirc_combined \; mycli --myclirc=~/.myclirc_combined \"\$\(mycli --list-dsn --myclirc=~/.myclirc_combined \| fzf --layout=reverse --prompt \'DSN\>\'\)\" \; rm ~/.myclirc_combined
 
