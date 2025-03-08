433998e09a1cffa4e5f93920e3dc38129d86c896
yuji naramoto <yuji.naramoto@playpark.work>
2025-03-09 06:36:21 +0900

    test: 💍 test

--- flake.nix
+++ flake.nix
@@ -2,21 +2,23 @@
   description = "Nix Darwin + Home Manager configuration";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin"; # nix-darwinのソース
-    home-manager.url = "github:nix-community/home-manager"; # Home Managerのソース
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs"; # nixpkgsに従う
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    home-manager.url = "github:nix-community/home-manager";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = { self, nixpkgs, nix-darwin, home-manager, ... }:
+    let
+      system = "aarch64-darwin";
+    in
     {
       darwinConfigurations = {
         MyMBP = nix-darwin.lib.darwinSystem {
-          system = "aarch64-darwin"; # Apple Siliconの場合
+          inherit system;
           modules = [
-            home-manager.darwinModules.home-manager # Home Managerを組み込む
-            ./darwin.nix # システム用設定（別ファイルにしてもOK）
+            home-manager.darwinModules.home-manager
+            ./darwin.nix
             {
-              # 以下、インラインでモジュール記述も可能
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.users.naramotoyuuji = import ./home.nix;
@@ -24,5 +26,13 @@
           ];
         };
       };
+
+      # ← 追加：packages 出力として darwinConfigurations をラップ
+      packages = {
+        "${system}" = {
+          darwinConfigurations = self.darwinConfigurations;
+        };
+      };
     };
 }
+
