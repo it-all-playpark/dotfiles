475aaf72f0cf8c28194a462a8a5deae9d121ee43
yuji naramoto <yuji.naramoto@playpark.work>
2025-03-09 04:38:10 +0900

    test: 💍 test

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "Nix Darwin + Home Manager configuration";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin";
-    home-manager.url = "github:nix-community/home-manager";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-darwin.url = "github:LnL7/nix-darwin"; # nix-darwinのソース
+    home-manager.url = "github:nix-community/home-manager"; # Home Managerのソース
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs"; # nixpkgsに従う
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = { self, nixpkgs, nix-darwin, home-manager, ... }:
@@ -13,17 +13,16 @@
         MyMBP = nix-darwin.lib.darwinSystem {
           system = "aarch64-darwin"; # Apple Siliconの場合
           modules = [
-            home-manager.darwinModules.home-manager
-            ./darwin.nix
+            home-manager.darwinModules.home-manager # Home Managerを組み込む
+            ./darwin.nix # システム用設定（別ファイルにしてもOK）
             {
-              # Home Manager の基本設定
+              # 以下、インラインでモジュール記述も可能
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.users.naramotoyuuji = import ./home.nix;
             }
-            # 最後に、home.homeDirectory を強制的に上書きするモジュール
-            { config, lib, ... }: {
-                home.homeDirectory = lib.mkForce "/Users/naramotoyuuji";
+            {
+              home-manager.users.naramotoyuuji.home.homeDirectory = "/Users/naramotoyuuji";
             }
           ];
         };
