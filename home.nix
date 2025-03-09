22c7418691a585cec984691a5c35e38bb28e76dd
yuji naramoto <yuji.naramoto@playpark.work>
2025-03-09 20:51:45 +0900

    feat: 🎸 .fish -> .nix

--- home.nix
+++ home.nix
@@ -3,84 +3,83 @@
 {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
-  home.username = "naramotoyuuji";
-  home.homeDirectory = "/Users/naramotoyuuji";
+  home = {
+    username = "naramotoyuuji";
+    homeDirectory = "/Users/naramotoyuuji";
 
-  home.stateVersion = "24.05"; # Please read the comment before changing.
+    stateVersion = "24.05"; # Please read the comment before changing.
 
-  home.packages = with pkgs; [
-    act
-    bat
-    eza
-    fastfetch
-    fd
-    ffmpegthumbnailer
-    ffmpeg
-    fzf
-    gh
-    ghq
-    google-cloud-sdk
-    delta
-    jq
-    lastpass-cli
-    lazydocker
-    lazygit
-    mise
-    mycli
-    pandoc
-    poppler
-    procs
-    rip2
-    ripgrep
-    ripgrep-all
-    sd
-    starship
-    tbls
-    tldr
-    tree-sitter
-    unar
-    yazi
-    zoxide
-  ];
+    packages = with pkgs; [
+      act
+      bat
+      eza
+      fastfetch
+      fd
+      ffmpegthumbnailer
+      ffmpeg
+      fzf
+      gh
+      ghq
+      google-cloud-sdk
+      delta
+      jq
+      lastpass-cli
+      lazydocker
+      lazygit
+      mise
+      mycli
+      pandoc
+      poppler
+      procs
+      rip2
+      ripgrep
+      ripgrep-all
+      sd
+      starship
+      tbls
+      tldr
+      tree-sitter
+      unar
+      yazi
+      zoxide
+    ];
 
-  # シェル有効化など
-  programs.zsh.enable = false;
-  programs.fish = {
-    enable = true;
-    shellInit = builtins.readFile ./programs/fish/shellInit.fish;
-    functions = builtins.readFile ./programs/fish/functions.fish;
-    shellAbbrs = builtins.readFile ./programs/fish/shellAbbrs.fish;
-  };
-
-  programs.neovim = {
-    enable = true;
-    vimAlias = true;
-  };
-
-  home.file = {
-    ".gitconfig".source = ./settings/.gitconfig;
-    ".gitconfig.local.template".source = ./settings/.gitconfig.local.template;
-    ".zshrc".source = ./settings/.zshrc;
-    ".tmux.conf".source = ./settings/.tmux.conf;
-    ".myclirc".source = ./settings/.myclirc;
-    ".myclirc.local.template".source = ./settings/.myclirc.local.template;
-    ".config/fish/config.fish.local.template".source = ./settings/fish/config.fish.local.template;
-    ".config/nvim" = {
-      source = ./settings/nvim;
-      recursive = true;
-    };
-    ".config/mise" = {
-      source = ./settings/mise;
-      recursive = true;
-    };
-    ".config/yazi" = {
-      source = ./settings/yazi;
-      recursive = true;
-    };
-    ".warp" = {
-      source = ./settings/.warp;
-      recursive = true;
+    file = {
+      ".gitconfig".source = ./settings/.gitconfig;
+      ".gitconfig.local.template".source = ./settings/.gitconfig.local.template;
+      ".zshrc".source = ./settings/.zshrc;
+      ".tmux.conf".source = ./settings/.tmux.conf;
+      ".myclirc".source = ./settings/.myclirc;
+      ".myclirc.local.template".source = ./settings/.myclirc.local.template;
+      ".config/fish/config.fish.local.template".source = ./settings/fish/config.fish.local.template;
+      ".config/nvim" = {
+        source = ./settings/nvim;
+        recursive = true;
+      };
+      ".config/mise" = {
+        source = ./settings/mise;
+        recursive = true;
+      };
+      ".config/yazi" = {
+        source = ./settings/yazi;
+        recursive = true;
+      };
+      ".warp" = {
+        source = ./settings/.warp;
+        recursive = true;
+      };
     };
   };
+
+  programs = {
+    # シェル有効化など
+    zsh.enable = false;
+    fish = builtins.readFile ./programs/fish.nix;
+    neovim = {
+      enable = true;
+      vimAlias = true;
+    };
+  };
+
 
 }
