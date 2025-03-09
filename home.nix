856ab915e2d602e1bf8d6e8013aa6fc27c773682
yuji naramoto <yuji.naramoto@playpark.work>
2025-03-09 10:41:30 +0900

    feat: 🎸 add nix path

--- home.nix
+++ home.nix
@@ -48,11 +48,13 @@
 
 
   # シェル有効化など
+  programs.zsh.enable = false;
+
   programs.fish = {
     enable = true;
     shellInit = ''
       # PATH設定
-      fish_add_path ~/.nix-profile/bin /opt/homebrew/bin /opt/homebrew/sbin /usr/bin/php ~/ghq/github.com/astj/ghq-migrator ~/google-cloud-sdk/bin ~/Library/Android/sdk ~/.local/share/mise/shims
+      fish_add_path ~/.nix-profile/bin /nix/var/nix/profiles/default/bin /opt/homebrew/bin /opt/homebrew/sbin /usr/bin/php ~/ghq/github.com/astj/ghq-migrator ~/google-cloud-sdk/bin ~/Library/Android/sdk ~/.local/share/mise/shims
 
       starship init fish | source
       zoxide init fish | source
@@ -169,6 +171,6 @@
 
 
   # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+  # programs.home-manager.enable = true;
 
 }
