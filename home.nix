ccaa57b5bdaf019f303e759a32ef5b9a098bec9e
yuji naramoto <yuji.naramoto@playpark.work>
2025-03-10 11:53:57 +0900

    feat: 🎸 put git settings under nix control

--- home.nix
+++ home.nix
@@ -45,12 +45,11 @@
     ];
 
     file = {
-      ".gitconfig".source = ./settings/.gitconfig;
-      ".gitconfig.local.template".source = ./settings/.gitconfig.local.template;
       ".zshrc".source = ./settings/.zshrc;
       ".tmux.conf".source = ./settings/.tmux.conf;
       ".myclirc".source = ./settings/.myclirc;
       ".myclirc.local.template".source = ./settings/.myclirc.local.template;
+      ".config/config.local.template".source = ./settings/git/config.local.template;
       ".config/fish/config.fish.local.template".source = ./settings/fish/config.fish.local.template;
       ".config/nvim" = {
         source = ./settings/nvim;
@@ -166,6 +165,36 @@
     neovim = {
       enable = true;
       vimAlias = true;
+    };
+    git = {
+      enable = true;
+      includes = [
+        { path = "config.local"; }
+      ];
+      delta = {
+        enable = true;
+        options = {
+          features = "decorations";
+          side-by-side = true;
+          interactive = {
+            keep-plus-minus-markers = false;
+            diff-filter = "delta --color-only --features=interactive";
+          };
+          decorations = {
+            commit-decoration-style = "blue ol";
+            commit-style = "raw";
+            file-style = "omit";
+            hunk-header-decoration-style = "blue box";
+            hunk-header-file-style = "red";
+            hunk-header-line-number-style = "#067a00";
+            hunk-header-style = "file line-number syntax";
+          };
+        };
+      };
+      extraConfig = {
+        init.default-branch = "main";
+        hub.protocol = "ssh";
+      };
     };
   };
 
