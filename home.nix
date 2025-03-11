c8b581a6c83785b4d6e1ba668e6f5122820e78f7
yuji naramoto <yuji.naramoto@playpark.work>
2025-03-12 06:12:51 +0900

    feat: 🎸 add home/default.nix

--- home.nix
+++ home.nix
@@ -1,67 +1,10 @@
 { config, pkgs, ... }:
-
+let
+  home-default = import ./home/default.nix { inherit pkgs; };
+in
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home = {
-    username = "naramotoyuuji";
-    homeDirectory = "/Users/naramotoyuuji";
-
-    stateVersion = "24.05"; # Please read the comment before changing.
-
-    packages = with pkgs; [
-      act
-      bat
-      eza
-      fastfetch
-      fd
-      ffmpegthumbnailer
-      ffmpeg
-      fzf
-      gh
-      ghq
-      google-cloud-sdk
-      jq
-      lastpass-cli
-      lazydocker
-      lazygit
-      mise
-      mycli
-      pandoc
-      poppler
-      procs
-      rip2
-      ripgrep
-      ripgrep-all
-      sd
-      starship
-      tbls
-      tldr
-      zoxide
-    ];
-
-    file = {
-      ".tmux.conf".source = ./settings/.tmux.conf;
-      ".myclirc".source = ./settings/.myclirc;
-      ".myclirc.local.template".source = ./settings/.myclirc.local.template;
-      ".config/git/config.local.template".source = ./settings/git/config.local.template;
-      ".config/fish/config.fish.local.template".source = ./settings/fish/config.fish.local.template;
-      ".config/nvim" = {
-        source = ./settings/nvim;
-        recursive = true;
-      };
-      ".config/mise" = {
-        source = ./settings/mise;
-        recursive = true;
-      };
-      ".warp" = {
-        source = ./settings/.warp;
-        recursive = true;
-      };
-    };
-  };
-
   imports = [
+    home-default
     ./programs/fish.nix
     ./programs/zsh.nix
     ./programs/git.nix
---
+++ home/default.nix
@@ -0,0 +1,60 @@
+{ pkgs, ... }:
+{
+  home = {
+    username = "naramotoyuuji";
+    homeDirectory = "/Users/naramotoyuuji";
+
+    stateVersion = "24.05"; # Please read the comment before changing.
+
+    packages = with pkgs; [
+      act
+      bat
+      eza
+      fastfetch
+      fd
+      ffmpegthumbnailer
+      ffmpeg
+      fzf
+      gh
+      ghq
+      google-cloud-sdk
+      jq
+      lastpass-cli
+      lazydocker
+      lazygit
+      mise
+      mycli
+      pandoc
+      poppler
+      procs
+      rip2
+      ripgrep
+      ripgrep-all
+      sd
+      starship
+      tbls
+      tldr
+      zoxide
+    ];
+
+    file = {
+      ".tmux.conf".source = ./settings/.tmux.conf;
+      ".myclirc".source = ./settings/.myclirc;
+      ".myclirc.local.template".source = ./settings/.myclirc.local.template;
+      ".config/git/config.local.template".source = ./settings/git/config.local.template;
+      ".config/fish/config.fish.local.template".source = ./settings/fish/config.fish.local.template;
+      ".config/nvim" = {
+        source = ./settings/nvim;
+        recursive = true;
+      };
+      ".config/mise" = {
+        source = ./settings/mise;
+        recursive = true;
+      };
+      ".warp" = {
+        source = ./settings/.warp;
+        recursive = true;
+      };
+    };
+  };
+}
