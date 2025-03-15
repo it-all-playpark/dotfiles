49145dd5c927c411d4b39dd7b218f63dd133d3b6
yuji naramoto <yuji.naramoto@playpark.work>
2025-03-15 13:02:17 +0900

    feat: 🎸 add copilot models

--- home-manager/home/file/nvim/lua/plugins/avante.lua
+++ home-manager/home/file/nvim/lua/plugins/avante.lua
@@ -8,7 +8,10 @@ return {
     provider = "copilot",
     auto_suggestions_provider = "copilot",
     copilot = {
+      -- model = "claude-3.5-sonnet",
       model = "claude-3.7-sonnet",
+      -- model = "gpt-o1",
+      -- model = "gpt-o3",
     },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
