15ce3de527f7890a9cac7b7cd1acb1cd8ea9c7e6
yuji naramoto <yuji.naramoto@playpark.work>
2025-05-26 17:46:05 +0900

    fix: 🐛 typo

--- home-manager/home/file/nvim/lua/plugins/lazyvim.lua
+++ home-manager/home/file/nvim/lua/plugins/lazyvim.lua
@@ -1,6 +1,6 @@
 return {
   "LazyVim/LazyVim",
   opts = {
-    colowrscheme = "catppuccin",
+    colorscheme = "catppuccin",
   },
 }
--- home-manager/home/file/nvim/lua/plugins/nvim-surround.lua
+++ home-manager/home/file/nvim/lua/plugins/nvim-surround.lua
@@ -6,14 +6,14 @@ return {
     require("nvim-surround").setup({
       -- Configuration here, or leave empty to use defaults
       keymaps = {
-        inmert = "<C-g>m",
-        inmert_line = "<C-g>M",
+        insert = "<C-g>m",
+        insert_line = "<C-g>M",
         normal = "ym",
         normal_cur = "ymm",
         normal_line = "yM",
         normal_cur_line = "yMM",
-        vimual = "M",
-        vimual_line = "gM",
+        visual = "M",
+        visual_line = "gM",
         delete = "dm",
         change = "cm",
         change_line = "cM",
