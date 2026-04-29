ad7b327c64a99c46f1df9f8468e6a62d172259fc
yuji naramoto <yuji.naramoto@playpark.work>
2026-04-29 11:16:05 +0900

    refactor(zsh): GLOBAL_RCS を無効化し zoxide/mise を明示初期化
    eval ベースの初期化を取りやめ、zoxide の関数定義と
    chpwd_functions 登録、mise shims の PATH 追加を直接記述。
    yy 関数は他所へ移管予定のため削除。

--- home-manager/programs/zsh.nix
+++ home-manager/programs/zsh.nix
@@ -13,21 +13,33 @@ in
       ${shellCommon.getPathConfig.zshLinux}
     '';
     envExtra = ''
-      # yaziでカレントディレクトリを変更
-      function yy() {
-      	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
-      	yazi "$@" --cwd-file="$tmp"
-      	if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-      		cd -- "$cwd"
-      	fi
-      	rm -f -- "$tmp"
-      }
+      unsetopt GLOBAL_RCS
+    '';
+    initContent = ''
       # terraformをopenTofuで代用
       function terraform() tofu
 
-      eval "$(zoxide init zsh)"
-      eval "$(starship init zsh)"
-      eval "$(mise activate zsh)"
+      function __zoxide_hook() {
+        command zoxide add -- "$PWD" >/dev/null 2>&1
+      }
+      function z() {
+        if [[ "$#" -eq 0 ]]; then
+          builtin cd -- "$HOME"
+        elif [[ "$#" -eq 1 && ( -d "$1" || "$1" == "-" || "$1" =~ ^[-+][0-9]+$ ) ]]; then
+          builtin cd -- "$1"
+        else
+          local result
+          result="$(command zoxide query --exclude "$PWD" -- "$@")" && builtin cd -- "$result"
+        fi
+      }
+      function zi() {
+        local result
+        result="$(command zoxide query --interactive -- "$@")" && builtin cd -- "$result"
+      }
+      typeset -ga chpwd_functions
+      chpwd_functions=("''${(@)chpwd_functions:#__zoxide_hook}" __zoxide_hook)
+
+      path=("$HOME/.local/share/mise/shims" $path)
     '';
     shellAliases = common.shellSortcuts;
   };
