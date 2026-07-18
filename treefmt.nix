013a0afed94ce6e5272eff437e6b2c6ba203d3a2
yuji naramoto <yuji.naramoto@playpark.work>
2026-07-18 09:49:03 +0900

    fix: json-sort-cli から Zed の JSONC ファイルを除外
    nix fmt 実行で home-manager/home/file/zed/keymap.json のコメント
    (大西配列 tnrs の説明など) が全て消失することが判明したため、
    json-sort-cli の対象から Zed の keymap.json / settings.json を除外する。

--- treefmt.nix
+++ treefmt.nix
@@ -15,5 +15,8 @@
     enable = true;
     autofix = true;
     insert-final-newline = true;
+    # Zed の keymap.json / settings.json はコメント付き JSON (JSONC) のため
+    # 厳密な JSON パーサーを使うフォーマッタにかけるとコメントが消えてしまう
+    excludes = [ "home-manager/home/file/zed/*.json" ];
   };
 }
